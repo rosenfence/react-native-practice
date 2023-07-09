e2eb1d505c2a5b8d00f3641d96bd650c1d4f0b68
rosenfence <rosenfence@gmail.com>
2023-07-09 22:24:55 +0900

    Core component 개념에 대해

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
+      <Text>New Text</Text>
+      <Button title='Tap me!' />
     </View>
   );
 }
